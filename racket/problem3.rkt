6723f943778c6bec61f33be3124a5e58c4452d8e
Yee Fay Lim <yeefay.lim@gmail.com>
2012-01-27 07:59:15 -0600

    Edited problem3.rkt

--- racket/problem3.rkt
+++ racket/problem3.rkt
@@ -3,15 +3,20 @@
 
 (define small 13195)
 (define max 600851475143)
+
 (define primes (cons 2 '()))
+
 (define unsieved 
   (rest (build-list (quotient small 2) 
                     (lambda (x) (+ (* x 2) 1)))))
+
+(define (find-idx lst elm)
+  (define (find-idx-with-accum lst elm i)
+    (cond [(empty? lst) #f]
+          [(= (first lst) elm) i]
+          [else (find-idx-with-accum (rest lst) elm (+ i 1))]))
+  (find-idx-with-accum lst elm 0))
+
 (define (sieve prms lst)
-  (let ([curr-prm-sqr (sqr (first lst))]))
-  (define (find-idx lst elm)
-    (define (find-idx-with-accum lst elm i)
-      (cond [(empty? lst) #f]
-            [(= (first lst) elm) i]
-            [else (find-idx-with-accum (rest lst) elm i+1)]))
-    (find-idx-with-accum lst curr-prm-sqr 0)))
+  (let ([curr-prm-sqr (sqr (first lst))])
+    (find-idx lst curr-prm-sqr)))
