f98300cdfb4913544d3e2bd35b734e0083db459a
Yee Fay Lim <yeefay.lim@gmail.com>
2012-01-30 03:54:01 -0600

    Edited problem3.rkt

--- racket/problem3.rkt
+++ racket/problem3.rkt
@@ -17,6 +17,17 @@
           [else (find-idx-with-accum (rest lst) elm (+ i 1))]))
   (find-idx-with-accum lst elm 0))
 
-(define (sieve prms lst)
-  (let ([curr-prm-sqr (sqr (first lst))])
-    (find-idx lst curr-prm-sqr)))
+(define (sieve lst prms)
+  (let* ([curr-prm-sqr (sqr (first lst))]
+         [curr-prm-sqr-idx (find-idx lst curr-prm-sqr)]
+         [lst->prms (take lst curr-prm-sqr-idx)]
+         [remaining-lst (drop lst curr-prm-sqr-idx)]
+         [not-divisible (lambda (x) (andmap 
+                                     (lambda (y) (not (= (modulo x y) 0))) 
+                                     lst->prms))]
+         [new-lst (filter not-divisible remaining-lst)]
+         [new-prms (append prms lst->prms)])
+    (sieve new-prms new-lst)))
+
+(sieve unsieved primes)
+         
