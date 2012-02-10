69a231b7c5ccc1c7e62f3c91f65d2794a4f101dd
Yee Fay Lim <yeefay.lim@gmail.com>
2012-02-10 01:04:14 -0600

    Problem 5 done

--- racket/problem5.rkt
+++ racket/problem5.rkt
@@ -1,11 +1,16 @@
 #lang racket
 
-;; Incomplete first-draft solution
-
 ;; 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
 ;; What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 (define (smallest-div-by-list-nums lst num)
-  (cond ([(empty? lst) num]
-         [else 
-(rest (rest (build-list 21 values)))
+  (cond [(empty? lst) num]
+        [else (let ([curr-factor (first lst)])
+                (smallest-div-by-list-nums
+                 (remove* '(1) (for/list ([i lst]) 
+                                 (if (= (modulo i curr-factor) 0)
+                                     (/ i curr-factor) i))) 
+                 (* num curr-factor)))]))
+
+(smallest-div-by-list-nums (rest (rest (build-list 11 values))) 1)
+(smallest-div-by-list-nums (rest (rest (build-list 21 values))) 1)
