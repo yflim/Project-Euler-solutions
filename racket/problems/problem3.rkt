bdc8b7a2b1964d85525850f7d1091164357f41c5
Yee Fay Lim <yeefay.lim@gmail.com>
2012-02-12 15:56:00 -0600

    Created problems/ folder, and moved problem*.rkt into it

--- racket/problems/problem3.rkt
+++ racket/problems/problem3.rkt
@@ -1,7 +1,10 @@
 #lang racket
 
+(require "../lib/factoring.rkt")
+
 ;; The prime factors of 13195 are 5, 7, 13 and 29.
 ;; What is the largest prime factor of the number 600851475143?
+
 
 (define (largest-prime-factor num)
   (let* ([sqrt-num (sqrt num)])
@@ -15,7 +18,11 @@
         (cond [(not (= bigfac num))
                (largest-prime-factor-gteq bigfac bigfac-complement)]
               [else num])))
-    (largest-prime-factor-gteq num 3)))
+    (cond [(even? num) 
+           (let ([odd (largest-odd-factor num)])
+             (cond [(> odd 1) (largest-prime-factor-gteq odd 3)]
+                   [else 2]))]
+          [else (largest-prime-factor-gteq num 3)])))
 
 (define big 600851475143)
 (time (largest-prime-factor big))
