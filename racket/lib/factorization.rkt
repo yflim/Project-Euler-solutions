40556e3e8c6dff464a1d8ec4e9230e6e07778c90
Yee Fay Lim <yeefay.lim@gmail.com>
2012-02-12 15:57:31 -0600

    Created folder lib; factored out factorization code into module factorization in lib/

---
+++ racket/lib/factorization.rkt
@@ -0,0 +1,35 @@
+#lang racket
+
+(provide (all-defined-out))
+
+(define (largest-factor-gteq odd complement)
+  (cond [(> complement (sqrt odd)) odd]
+        [(= (modulo odd complement) 0) (/ odd complement)]
+        [else (largest-factor-gteq odd (+ complement 2))]))
+
+(define (largest-odd-factor even)
+  (cond [(even? even) (largest-odd-factor (/ even 2))]
+        [else even]))
+
+(define (to-list power-of-two lst)
+  (cond [(= power-of-two 1) lst]
+        [else (to-list (/ power-of-two 2) (append lst (list 2)))]))
+
+(define (prime-factors num)
+  (define (prime-factors-rec num lst)
+    (let* ([bigfac (largest-factor-gteq num (last lst))]
+           [bigfac-complement (/ num bigfac)])
+      (cond [(not (= bigfac num))
+             (prime-factors-rec bigfac (append lst (list bigfac-complement)))]
+            [else (append lst (list num))])))
+  (cond [(even? num) (let ([odd (largest-odd-factor num)])
+                       (prime-factors-rec odd (to-list (/ num odd) '())))]
+        [else (let ([fac (largest-factor-gteq num 3)])
+                (prime-factors-rec fac (list (/ num fac))))]))
+
+(define (largest-prime-factor-gteq num min-complement)
+  (let* ([bigfac (largest-factor-gteq num min-complement)]
+         [bigfac-complement (/ num bigfac)])
+    (cond [(not (= bigfac num))
+           (largest-prime-factor-gteq bigfac bigfac-complement)]
+          [else num])))
